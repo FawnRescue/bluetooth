4f2465383e89297d37808112e4f28fb1f77c25b2
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2024-01-29 22:13:57 +0100

    feat: remove log statements

--- bluetooth.js
+++ bluetooth.js
@@ -1,53 +1,49 @@
 const bleno = require('@abandonware/bleno');
 
-// Define a custom service and characteristic
-const PrimaryService = bleno.PrimaryService;
-const Characteristic = bleno.Characteristic;
+// Define a custom Bluetooth service and characteristic
+const BluetoothService = bleno.PrimaryService;
+const BluetoothCharacteristic = bleno.Characteristic;
 
-const MyCustomCharacteristic = new Characteristic({
-    uuid: '502E4974-FC68-42B1-8402-33DAF244E47C', // Custom UUID for the characteristic
-    properties: ['read', 'write'], // Define properties (read, write, notify)
-    onWriteRequest: function(data, offset, withoutResponse, callback) {
-        console.log('Data received: ' + data.toString()); // Log the received data
-        callback(this.RESULT_SUCCESS); // Indicate success
+const DataTransferCharacteristic = new BluetoothCharacteristic({
+    uuid: '502E4974-FC68-42B1-8402-33DAF244E47C', // Unique identifier for the characteristic
+    properties: ['read', 'write'], // Enable read and write capabilities
+    onWriteRequest: function (receivedData, offset, withoutResponse, callback) {
+        console.log(receivedData.toString()); // Log the received data
+        callback(this.RESULT_SUCCESS); // Signal successful write operation
+        process.exit()
     }
 });
 
-const myService = new PrimaryService({
-    uuid: 'F08A484F-8957-4F21-AA95-B58252D8D708', // Custom UUID for the service
-    characteristics: [MyCustomCharacteristic]
+const CustomService = new BluetoothService({
+    uuid: 'F08A484F-8957-4F21-AA95-B58252D8D708', // Unique identifier for the service
+    characteristics: [DataTransferCharacteristic] // List of characteristics in this service
 });
 
-bleno.on('stateChange', function(state) {
-    console.log('State change: ' + state);
-    if (state === 'poweredOn') {
-        bleno.startAdvertising('FawnRescue', [myService.uuid]);
+bleno.on('stateChange', function (bluetoothState) {
+    if (bluetoothState === 'poweredOn') {
+        bleno.startAdvertising('FawnRescue', [CustomService.uuid]);
     } else {
         bleno.stopAdvertising();
     }
 });
 
-bleno.on('advertisingStart', function(error) {
-    console.log('Advertising start: ' + (error ? 'error ' + error : 'success'));
-    if (!error) {
-        bleno.setServices([myService]);
+bleno.on('advertisingStart', function (advertisingError) {
+    if (!advertisingError) {
+        bleno.setServices([CustomService]);
     }
 });
 
-// Handling disconnection
+// Handle client disconnection events
 bleno.on('disconnect', (clientAddress) => {
-    console.log(`Client disconnected: ${clientAddress}`);
-    // Reset the BLE hardware to a known state
+    // Reset Bluetooth to a known state
     bleno.stopAdvertising();
-    bleno.startAdvertising('FawnRescue', [myService.uuid]);
+    bleno.startAdvertising('FawnRescue', [CustomService.uuid]);
 });
 
-// Optional: Handle new connections
+// Optionally handle new client connections
 bleno.on('accept', (clientAddress) => {
-    console.log(`Client connected: ${clientAddress}`);
     setTimeout(() => {
-        console.log('No data received for 5 seconds, disconnecting...');
-        bleno.disconnect(); // Disconnect the client
-        bleno.startAdvertising('FawnRescue', [myService.uuid]);
+        bleno.disconnect(); // Disconnect the inactive client
+        bleno.startAdvertising('FawnRescue', [CustomService.uuid]);
     }, 5000);
 });
